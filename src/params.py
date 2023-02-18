5c699fb93b3719578cd6de048b0ea7deeaac1313
Sang Soon Oh <OhS2@cardiff.ac.uk>
2023-02-18 20:53:45 +0000

    Fourier coeff works

--- src/params.py
+++ src/params.py
@@ -5,12 +5,12 @@ import math as m
 class Params():
 	def __init__(self,
 					Nx = 9, Ny =1, Nkx = 15, Nky= 5,
-					Lx = 1.2, Ly = 1,
-                    a = 1, b = 1/m.sqrt(3),
+					# Lx = 1.2, Ly = 1,
+                    a = 1, b=1/m.sqrt(3),
 					a1_norm = np.array([0.5*m.sqrt(3), 0.5]), a2_norm = np.array([0.5*m.sqrt(3), -0.5]), 
 					g1_norm = np.array([0.5, 0.5*m.sqrt(3)]), g2_norm = np.array([-0.5, 0.5*m.sqrt(3)]), 
-
-					dx = 0.05, dy = 0.05
+					dx = 0.05, dy = 0.05,
+					padding = 3
 					):
 		"""
 		Init the parameters of the system
@@ -20,21 +20,31 @@ class Params():
 		self.Nkx = Nkx
 		self.Nky = Nky
 		self.Nkx_max = m.floor(Nkx/2)
-		self.Nky_max = m.floor(Nky/2)		
+		self.Nky_max = m.floor(Nky/2)	
 
 		self.a = a
-		self.b = b
+		self.b = b ,
+
+		self.Lx = (Nx*3*b) - b + 2*padding
+		self.Ly = a
+
+		self.x_min = -0.5*self.Lx
+		self.x_max = 0.5*self.Lx
+
+
+		self.y_min = -0.5*self.Ly
+		self.y_max = 0.5*self.Ly
 
 		self.a1 = a*a1_norm
 		self.a2 = a*a2_norm
 
 		self.g1_norm = g1_norm
 		self.g2_norm = g2_norm
-		self.g1 = 2*m.pi/(a*Lx) *g1_norm
-		self.g2 = 2*m.pi/(a*Ly) *g2_norm
+		self.g1 = 2*m.pi/(self.Lx) *g1_norm
+		self.g2 = 2*m.pi/(self.Ly) *g2_norm
 
-		self.G1 = 2*m.pi/(a*Lx)*np.array([1, 0]) 
-		self.G2 = 2*m.pi/(a*Ly)*np.array([0, 1])
+		self.G1 = 2*m.pi/(self.Lx)*np.array([1, 0]) 
+		self.G2 = 2*m.pi/(self.Ly)*np.array([0, 1])
 
 		self.dx = dx
 		self.dy = dy
